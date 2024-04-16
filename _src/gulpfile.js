ffe3d840180ac15fd11741c8ff7b7a1dca625c70
siofokapartmanhazak <siofokapartmanhazak@gmail.com>
2024-04-16 14:00:32 +0200

    ok

--- _src/gulpfile.js
+++ _src/gulpfile.js
@@ -8,7 +8,7 @@ const fs = require('fs')
 const htmlmin = require('gulp-htmlmin')
 livereload = require('gulp-livereload')
 
-const VERSION = "16"
+const VERSION = "17"
 
 
 const inlinesource = require('gulp-inline-source');
